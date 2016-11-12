4324fa8c7c50b7303079fa05cc680c553e4a950e
Trevor Edwards <tre23@cornell.edu>
2016-11-12 09:56:29 -0500

    add views, jades

--- cafaapp/urls.py
+++ cafaapp/urls.py
@@ -6,4 +6,10 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^int/login$', views.loginuser, name='login'),
+    url(r'^int/dashboard', views.dash, name='dash'),
+    url(r'^int/new_contract', views.newcontract, name='newcontract'),
+    url(r'^int/view_contract', views.viewcontract, name='viewcontract'),
+    url(r'^ext/login$', views.loginext, name='loginexit'),
+    url(r'^ext/job$', views.job, name='job'),
+    url(r'^ext/jobupdated$', views.jobupdated, name='jobupdated'),
 ]
--- cafaapp/views.py
+++ cafaapp/views.py
@@ -11,9 +11,9 @@ from django.views.decorators.csrf import csrf_exempt
 #user.save()
 
 @csrf_exempt
-def loginuser(request):
+def loginext(request):
     if request.method == 'GET':
-        template = loader.get_template("cafaapp/templates/login.html")
+        template = loader.get_template("cafaapp/templates/login_ext.jade")
         return HttpResponse(template.render())
     elif request.method == 'POST':
         username = request.POST.get('username', False)
@@ -33,6 +33,55 @@ def loginuser(request):
             return HttpResponse(template.render())
 
 @csrf_exempt
+def dash(request):
+    template = loader.get_template("cafaapp/templates/int/dashboard.jade")
+    return HttpResponse(template.render())
+
+@csrf_exempt
+def loginuser(request):
+    if request.method == 'GET':
+        template = loader.get_template("cafaapp/templates/login.html")
+        return HttpResponse(template.render())
+    elif request.method == 'POST':
+        username = request.POST.get('username', False)
+        password = request.POST.get('pwd', False)
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            # Redirect to a success page.
+            print("Success")
+            template = loader.get_template("cafaapp/templates/int/dashboard.jade")
+            return HttpResponse(template.render())
+        else:
+            # Return an 'invalid login' error message.
+            #login(request, user) #TODO
+            print("FAIL")
+            template = loader.get_template("cafaapp/templates/login.html")
+            return HttpResponse(template.render())
+
+@csrf_exempt
 def index(request):
     template = loader.get_template("cafaapp/templates/index.jade")
     return HttpResponse(template.render())	
+
+@csrf_exempt
+def jobupdated(request):
+    template = loader.get_template("cafaapp/templates/ext/job_updated.jade")
+    return HttpResponse(template.render())
+
+@csrf_exempt
+def newcontract(request):
+    template = loader.get_template("cafaapp/templates/int/new_contract.jade")
+    return HttpResponse(template.render())
+
+
+@csrf_exempt
+def viewcontract(request):
+    template = loader.get_template("cafaapp/templates/int/view_contract.jade")
+    return HttpResponse(template.render())
+
+
+@csrf_exempt
+def job(request):
+    template = loader.get_template("cafaapp/templates/ext/do_job.jade")
+    return HttpResponse(template.render())
