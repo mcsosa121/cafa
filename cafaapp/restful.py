b8c1fab59288deb777540bb0a80da04e6cea51a6
Trevor Edwards <tre23@cornell.edu>
2016-11-13 03:53:48 +0000

    fix stuff

--- cafaapp/restful.py
+++ cafaapp/restful.py
@@ -37,7 +37,7 @@ def create_contract(request):
         zip = request.POST.get('zip', False)
         contract = House(lat=lat, lon=lon,street=street,city=city,state=state,zip=zip,img_docfile=docfile)
         contract.save()
-        return HttpResponseRedirect('views.dash')
+        return HttpResponseRedirect('/cafe/int/dashboard')
     raise Http404("u missin stuff!!!!")
 
 def house_cereal(houses):
